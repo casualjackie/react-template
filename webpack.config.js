f26caae7436dcea0c8526dba56aa1ae16706bb99
Evgeniy Polishchuk <68184643+CasualJackie@users.noreply.github.com>
2023-04-29 15:48:29 +0300

    added plugin transform react jsx

--- webpack.config.js
+++ webpack.config.js
@@ -128,6 +128,9 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              ["@babel/plugin-transform-react-jsx", { runtime: "automatic" }],
+            ],
           },
         },
       },
