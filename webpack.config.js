70835f615bf162bd799c1dad1d1d07b457d84313
Evgeniy Polishchuk <68184643+CasualJackie@users.noreply.github.com>
2023-05-03 18:02:35 +0300

    removed unnecesary code

--- webpack.config.js
+++ webpack.config.js
@@ -112,31 +112,6 @@ module.exports = {
         type: "asset/resource",
       },
       {
-        test: /\.js$/i,
-        exclude: /node_modules/,
-        use: jsLoaders(),
-      },
-      {
-        test: /\.(ts)$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-typescript"],
-          },
-        },
-      },
-      {
-        test: /\.(jsx)$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-      {
         test: /\.(js|ts|tsx)$/,
         exclude: /node_modules/,
         use: [
